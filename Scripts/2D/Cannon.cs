61d8d69544238852feafbd8ca1d62fc86ff4c8c1
Anton <manton2007@gmail.com>
2023-08-28 18:33:58 +0300

    Cannon fix

--- Scripts/2D/Cannon.cs
+++ Scripts/2D/Cannon.cs
@@ -8,46 +8,45 @@ namespace Zenitka.Scripts._2D
 	{
 
 		const float GunRotationSpeed = 1.0f;
-	    
-    	private Sprite2D _body;
-        private Sprite2D _gun;
-        private Node2D _bulletSpawn;
-
-        private float _targetAngle = -0.5f;
-
-        public override void _Ready()
-    	{
-    		_body = GetChild(0) as Sprite2D;
-            _gun = _body.GetChild(0) as Sprite2D;
-            _bulletSpawn = _gun.GetChild(0) as Node2D;
-        }
+		
+		private Sprite2D _body;
+		private Sprite2D _gun;
 
 
-        void RotateTo(float targetAngle)
-        {
-	        _targetAngle = targetAngle;
-        }
-        
-    	public override void _Process(double delta)
-        {
-	        if(Math.Abs(_gun.Rotation - _targetAngle) == 0)
-		        return;
-	        if (Math.Abs(_gun.Rotation - _targetAngle) < Math.Abs(GunRotationSpeed * (float)delta))
-	        {
-		        _gun.Rotation = _targetAngle;
-		        return;
-	        }
+		private float _targetAngle = -0.5f;
 
-	        if (_gun.Rotation > _targetAngle)
-	        {
-		        _gun.Rotation -= GunRotationSpeed * (float)delta ;
-	        }
-	        else
-	        {
-		        _gun.Rotation += GunRotationSpeed * (float)delta ;
-	        }
-        }
-    }
+		public override void _Ready()
+		{
+			_body = GetChild(0) as Sprite2D;
+			_gun = _body.GetChild(0) as Sprite2D;
+		}
+
+
+		void RotateTo(float targetAngle)
+		{
+			_targetAngle = targetAngle;
+		}
+		
+		public override void _Process(double delta)
+		{
+			if(Math.Abs(_gun.Rotation - _targetAngle) == 0)
+				return;
+			if (Math.Abs(_gun.Rotation - _targetAngle) < Math.Abs(GunRotationSpeed * (float)delta))
+			{
+				_gun.Rotation = _targetAngle;
+				return;
+			}
+
+			if (_gun.Rotation > _targetAngle)
+			{
+				_gun.Rotation -= GunRotationSpeed * (float)delta ;
+			}
+			else
+			{
+				_gun.Rotation += GunRotationSpeed * (float)delta ;
+			}
+		}
+	}
 
 }
 
