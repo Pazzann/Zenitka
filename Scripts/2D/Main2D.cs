ff220a31a2491fee8c474fd78fc60a24edfa282e
egorv <egor.vaskonyan@gmail.com>
2023-09-21 19:15:16 +0200

    Bullet bounding box fix

--- Scripts/2D/Main2D.cs
+++ Scripts/2D/Main2D.cs
@@ -51,7 +51,7 @@ namespace Zenitka.Scripts._2D
 			var startPos = GenerateTargetSpawnlocation(kind);
 			var endPos = GenerateTargetSpawnlocation(!kind);
 
-			target.GlobalPosition = startPos;
+			target.GlobalPosition = startPos - new Vector2(20f, 20f);
 			target.LinearVelocity = (endPos - startPos).Normalized() * TARGET_SPEED;
 
 			target.GravityScale = 0f;
