31ba3c28b74e40e4f5b8be13c4f297b264ddbe31
egorv <egor.vaskonyan@gmail.com>
2023-10-25 12:17:29 +0200

    fucking cannon fix

--- Scripts/2D/Main2D.cs
+++ Scripts/2D/Main2D.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Runtime.Intrinsics;
 using Zenitka.Scripts._2D.Targets;
 using Zenitka.Scripts.Math;
 using Zenitka.Scripts.UI;
@@ -9,7 +10,7 @@ namespace Zenitka.Scripts._2D
 {
 	public partial class Main2D : Node2D
 	{
-		private static float BARREL_LENGTH = 200f;
+		private static float BARREL_LENGTH = 64f;
 		private static float MUZZLE_SPEED = 2000f;
 		private static float TARGET_SPEED = 2000f;
 
@@ -36,24 +37,20 @@ namespace Zenitka.Scripts._2D
 			_bulletScene = GD.Load<PackedScene>("res://Prefabs/Bullet.tscn");
 		}
 
-		
-
 		private void OnCannonGunReady(float angleRad, Vector2 headPosition, float timeOfCollision)
 		{
 
 			var bullet = _bulletScene.Instantiate() as Bullet;
 			
-			bullet.SelfDestructionTime = timeOfCollision - 0.05f;
-			bullet.GlobalPosition = new Vector2(0.0f, 0.0f);
+			bullet.SelfDestructionTime = timeOfCollision - 0.1f;
+			bullet.GlobalPosition = Vector2.Zero;
 			bullet.GravityScale = 0f;
 			bullet.StartAngle = angleRad;
 
 			AddChild(bullet);
 			MoveChild(bullet, 0);
-			
-			
 
-			if (_firedBurstBulletCount++ < 10)
+			if (_firedBurstBulletCount++ < 3)
 				ToSignal(GetTree().CreateTimer(0.05f), SceneTreeTimer.SignalName.Timeout).OnCompleted(() =>
 				{
 				_cannon.RotateToAndSignal(angleRad + 0.02f, timeOfCollision);
@@ -78,7 +75,7 @@ namespace Zenitka.Scripts._2D
 			var (angle, timeOfCollision) = new Solver2D(
 				new CannonState2D(
 					Vector2.Zero,
-					BARREL_LENGTH,
+					0f,
 					_cannon.GetAngle(),
 					Settings.Settings2D.DefaultGun.AngularVelocity,
 					Settings.Settings2D.DefaultGun.BulletSpeed,
@@ -105,7 +102,6 @@ namespace Zenitka.Scripts._2D
 		private Vector2 GenerateTargetSpawnlocation()
 		{
 			var pos = new Vector2(0.0f, _anchor2.GlobalPosition.Y);
-
 
 			Random rand = new Random();
 			pos.X = (rand.Next(0, 2) == 0) ? _anchor1.GlobalPosition.X : _anchor2.GlobalPosition.X;
