a427cd60f1ec23b79482e6cd45ea715e2147bbc4
Kreker76 <140255268+Kreker76@users.noreply.github.com>
2023-10-25 08:56:44 +0300

    Camera Patch(2D and 3D)

--- Scripts/2D/Camera.cs
+++ Scripts/2D/Camera.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Camera : Camera2D
 {
+	private Button _button;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,12 +12,23 @@ public partial class Camera : Camera2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		_button = GetNodeOrNull<Button>("/root/Main2D/Button2");
 		if (Input.IsActionJustPressed("switch1"))
 		{
 			if (IsCurrent() == false)
 			{
 				MakeCurrent();
 			}
+		}
+		if (Input.IsActionJustPressed("cam_zoom_out_3d")&&Zoom.X>0.2f)
+		{
+			Zoom = Zoom / 1.3f;
+			Position = new Vector2(Position.X * 1.3f, Position.Y * 1.3f);
+		}
+		if (Input.IsActionJustPressed("cam_zoom_in_3d"))
+		{
+			Zoom = Zoom * 1.3f;
+			Position= new Vector2(Position.X/1.3f, Position.Y/1.3f);
 		}
 	}
 }
