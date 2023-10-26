3f0f128b9396c13541a64217741dc22a3048d9f2
egorv <egor.vaskonyan@gmail.com>
2023-10-26 17:00:58 +0200

    fixes

--- Scripts/2D/Targets/Rocket2.cs
+++ Scripts/2D/Targets/Rocket2.cs
@@ -9,7 +9,7 @@ namespace Zenitka.Scripts._2D.Targets
 
 		private CollisionShape2D _rocketCollision;
 
-		private float _acceleration;
+		private float _engineForce;
 
 		private float _currentFuel;
 
@@ -24,13 +24,17 @@ namespace Zenitka.Scripts._2D.Targets
 
 		
 		public float CurrentTime = 0.0f;
-		public float StartAngle = 0.0f;
+		public float StartAngle = Mathf.Pi;
+
+		public float ManeuveringEngineActivationDelaySec = 1f;
 
 		public bool IsExploded;
 
+		private bool _scoreUpdated = false;
+
 		public override void _Ready()
 		{
-			_acceleration = Settings.Settings2D.RocketGun.RocketForce;
+			_engineForce = Settings.Settings2D.RocketGun.RocketForce;
 			StartPosition = new Vector2(GlobalPosition.X, GlobalPosition.Y);
 			
 			Weight = Settings.Settings2D.RocketGun.RocketMassWithoutFuel + Settings.Settings2D.RocketTarget.FuelMass;
@@ -55,7 +59,11 @@ namespace Zenitka.Scripts._2D.Targets
 
 			if (!IsExploded)
 				Rotation = state.LinearVelocity.Normalized().Angle();
-			ConstantForce = Mass * (FollowTarget.GlobalPosition - GlobalPosition).Normalized() * _acceleration / Weight;
+			
+			if (CurrentTime > ManeuveringEngineActivationDelaySec)
+				ConstantForce = Mass * (FollowTarget.GlobalPosition - GlobalPosition).Normalized() * _engineForce / Weight;
+			else
+				ConstantForce = Mass * Vector2.Up * _engineForce / Weight; 
 		}
 
 		public override void _PhysicsProcess(double delta)
@@ -70,7 +78,7 @@ namespace Zenitka.Scripts._2D.Targets
 				if(IsExploded)
 					return;
 				_currentFuel = 0;
-				_acceleration = 0;
+				_engineForce = 0;
 				Destroy();
 			}
 			
@@ -86,15 +94,24 @@ namespace Zenitka.Scripts._2D.Targets
 			
 			IsExploded = true;
 		}
+
 		private void _on_body_entered(Node body)
 		{
 			(body as Target).Destroy();
 			Destroy();
-			_destroyedLabel.Text = (Int32.Parse(_destroyedLabel.Text)  + 1).ToString();
+
+			if (!_scoreUpdated) {
+				_destroyedLabel.Text = (Int32.Parse(_destroyedLabel.Text)  + 1).ToString();
+				_scoreUpdated = true;
+			}
 		}
 		private void _on_button_pressed()
 		{
 			Destroy();
+		}
+
+		private Vector2 PredictFutureTargetPosition(float t) {
+			return FollowTarget.GlobalPosition + (FollowTarget as RigidBody2D).LinearVelocity;
 		}
 
 	}
