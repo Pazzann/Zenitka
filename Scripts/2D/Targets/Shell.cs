4aef6d0d1c48d6b7576d5cf1fdf57db318c55f6f
Anton <manton2007@gmail.com>
2023-10-26 12:34:58 +0300

    Release 0.1.2

--- Scripts/2D/Targets/Shell.cs
+++ Scripts/2D/Targets/Shell.cs
@@ -5,18 +5,30 @@ namespace Zenitka.Scripts._2D.Targets
 {
 	public partial class Shell : Target
 	{
+		private AnimatedSprite2D _animation;
+		private CollisionShape2D _rocketCollision;
 		public override void _Ready()
 		{
 			StartPosition = new Vector2(Position.X, Position.Y);
 			ConstantAcceleration = 0;
 			
+			_animation = GetChild(1) as AnimatedSprite2D;
+			
 			Weight = Settings.Settings2D.DefaultTarget.Mass;
 			DragCoefficient = Settings.Settings2D.DefaultTarget.AirResistance;
 			StartVelocity = Settings.Settings2D.DefaultTarget.Velocity;
+			
+			_animation.Play("fly");
+			_rocketCollision = GetChild(0) as CollisionShape2D;
 		}
 		public override void Destroy()
 		{
-			QueueFree();
+			_rocketCollision.Disabled = true;
+			_animation.Play("explode");
+			
+			_animation.Connect("animation_looped", Callable.From(QueueFree));
+			
+			IsExploded = true;
 		}
 	}
 }
