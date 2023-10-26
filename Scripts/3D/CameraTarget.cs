0d49b5c0fc243a0fb8296fce6e67bf87a00a6c25
Kreker76 <140255268+Kreker76@users.noreply.github.com>
2023-10-26 12:24:01 +0300

    Camera3D

--- Scripts/3D/CameraTarget.cs
+++ Scripts/3D/CameraTarget.cs
@@ -8,8 +8,10 @@ public partial class CameraTarget : Camera3D
 
 	private float _zoom = 1f;
 	private Target _target;
+	private Node3D _rocket;
 	private Vector3 _targetLinearVelocityNormalized;
 	private Basis _targetBasis;
+	private bool _first = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,37 +21,44 @@ public partial class CameraTarget : Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-			_target = GetNodeOrNull<Target>("/root/Main3D/Target");
+		_target = GetNodeOrNull<Target>("/root/Main3D/Target");
+		_rocket = GetNodeOrNull<Node3D>("/root/Main3D/Target/Rocket");
 
-			if (_target != null)
+
+		if (_target != null)
+		{
+
+			if (Input.IsActionPressed("cam_zoom_in_3d") && _zoom - ZOOM_SPEED > 0.3f)
 			{
+				_zoom -= ZOOM_SPEED;
+			}
 
-			 	if (Input.IsActionPressed("cam_zoom_in_3d") && _zoom - ZOOM_SPEED > 0.3f)
-			 	{
-			 		_zoom -= ZOOM_SPEED;
-			 	}
-
-			 	if (Input.IsActionPressed("cam_zoom_out_3d") && _zoom + ZOOM_SPEED < 10f)
-			 	{
-			 		_zoom += ZOOM_SPEED;
-			 	}
-				_targetLinearVelocityNormalized = _target.LinearVelocity.Normalized();
-				Basis = _target.Basis;
-				Position = _target.Position;
-				if (_targetLinearVelocityNormalized != new Vector3(0, 0, 0))
-				{
-					Translate(new Vector3(-_targetLinearVelocityNormalized.X * 5 * _zoom, 10 * _zoom, -_targetLinearVelocityNormalized.Z * 5 * _zoom));
-				}
-				else
-				{ Translate(new Vector3(-5 * _zoom, 10 * _zoom, -5 * _zoom)); }
-				Transform = Transform.LookingAt(_target.Position, Vector3.Up);
-
-
+			if (Input.IsActionPressed("cam_zoom_out_3d") && _zoom + ZOOM_SPEED < 10f)
+			{
+				_zoom += ZOOM_SPEED;
+			}
+			_targetLinearVelocityNormalized = _target.LinearVelocity.Normalized();
+			Basis = _target.Basis;
+			Position = _target.Position;
+			if (_targetLinearVelocityNormalized != new Vector3(0, 0, 0))
+			{
+				Translate(new Vector3(-_targetLinearVelocityNormalized.X * 5 * _zoom, 10 * _zoom, -_targetLinearVelocityNormalized.Z * 5 * _zoom));
+			}
+			else
+			{ Translate(new Vector3(-5 * _zoom, 10 * _zoom, -5 * _zoom)); }
+			Transform = Transform.LookingAt(_target.Position, Vector3.Up);
+			if (_target.LinearVelocity != new Vector3(0, 0, 0))
+			{
+					_rocket.Transform=_rocket.Transform.LookingAt(-_target.LinearVelocity*3,Vector3.Up);
+			}
 		}
+
 	}
 	private void _on_target_spawn_timer_timeout()
 	{
 		_target = GetNode<Target>("/root/Main3D/Target");
+		_rocket = GetNode<Node3D>("/root/Main3D/Target/Rocket");
+
 		if (_target != null)
 		{
 		}
