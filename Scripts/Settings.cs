10eb4a1ca267c823ae105c254de7b3a7dd93c541
egorv <egor.vaskonyan@gmail.com>
2024-01-08 14:23:46 +0100

    asdadsasdd

--- Scripts/Settings.cs
+++ Scripts/Settings.cs
@@ -96,7 +96,7 @@ public partial class Settings : Node
 				FuelMass = 4;
 				FuelCost = 0.1f;
 				AngularVelocity = 4.0f;
-				SideEActivationDelay = 0.1f;
+				SideEActivationDelay = 1f;
 			}
 		}
 
